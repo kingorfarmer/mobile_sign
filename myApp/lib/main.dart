467fd11a3292cdb5345c05168bafee42fc062bf2
Hai <hou.nghoanghai@gmail.com>
2020-11-30 11:18:55 +0700

    30/11/2020

---
+++ myApp/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'MyApp.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: IntroScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class IntroScreen extends StatefulWidget {
+  IntroScreen({Key key}) : super(key: key);
+
+  @override
+  IntroScreenState createState() => new IntroScreenState();
+}
